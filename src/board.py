367e1deaa14205372a96aa4e1252b6abb31b2ba0
suey <ho055@csiro.au>
2025-01-17 13:10:19 +1100

    initialize board

--- src/board.py
+++ src/board.py
@@ -1,7 +1,25 @@
-from property import Property
+from src.property import Property
 
 class Board:
+    """ Board class to represent the board layout
+    """
     def __init__(self, positions) -> None:
         self._positions = []
+        for i,position in enumerate(positions):
+            name = position["name"]
+            property_type = position["type"]
+            if property_type == "go":
+                price = None
+                colour = None
+            else:
+                price = position["price"]
+                colour =  position["colour"]
+            new_property = Property(name, price, colour, property_type, i)
+            self._positions.append(new_property)
     def get_property(self, position):
-        pass
+        """Getter of property
+
+        Args:
+            position (int): position index of the property on the board
+        """
+        return self._positions[position]
